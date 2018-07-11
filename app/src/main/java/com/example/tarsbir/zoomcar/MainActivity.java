c615f0b0568bf097bd5def3d619fbaa235076270
Tarsbir Singh <tarsbirsingh@gmail.com>
2018-07-11 11:36:44 +0530

    add tab with on select

--- app/src/main/java/com/example/tarsbir/zoomcar/MainActivity.java
+++ app/src/main/java/com/example/tarsbir/zoomcar/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.tarsbir.zoomcar;
 
+import android.content.Intent;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Thread bk = new Thread() {
+            public void run() {
+                try {
+                    sleep(3 * 1000);
+
+                    Intent i = new Intent(getApplicationContext(), Main2Activity.class);
+                    startActivity(i);
+                    finish();
+                } catch (Exception e) {
+                }
+            }
+        };
+        bk.start();
     }
 }
